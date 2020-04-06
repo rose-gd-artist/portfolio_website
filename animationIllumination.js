95b75653bba527be2d37a68f0b84509de84a7590
rose-gd-artist <61298013+rose-gd-artist@users.noreply.github.com>
2020-04-05 22:59:52 -0400

    finished save our oceans logo page and put a placeholder in save oceans website page

---
+++ animationIllumination.js
@@ -0,0 +1,74 @@
+///////// dropdown menu controls /////////////
+
+document.getElementById("menu1").onclick = function() {menuOpen()};
+
+function menuOpen(){
+    document.getElementById("menu1").style.display = "none";
+    document.getElementById("menu2").style.display = "block";
+    document.getElementById("dropdown").style.display = "block";
+}
+
+document.getElementById("menu2").onclick = function() {menuClose()};
+
+function menuClose(){
+    document.getElementById("menu1").style.display = "block";
+    document.getElementById("menu2").style.display = "none";
+    document.getElementById("dropdown").style.display = "none";
+}
+
+
+
+/////////////////// all my variables ///////////////////////
+
+let closeModalbutton = document.getElementById("closeModalbutton");
+
+let contactModal = document.getElementById("contactModal");
+
+let menuContact = document.getElementById("menuContact");
+
+let contactButtonModal = document.getElementById("contactButtonModal");
+
+
+
+//////////// Description drop down ////////////////
+
+let infoClose = document.getElementById("infoClose");
+
+let info_button = document.getElementById("info_button");
+
+document.getElementById("info_button").onclick = function() {descriptionOpen()};
+
+function descriptionOpen(){
+    if(infoClose.style.display === "none"){
+        document.getElementById("infoClose").style.display = "block";
+        document.getElementById("info_modal_godZ").style.display = "block";
+    } else {
+       document.getElementById("infoClose").style.display = "none";
+       document.getElementById("info_modal_godZ").style.display = "none"; 
+    }
+}
+
+
+
+
+
+
+//////////// contact modal /////////////////
+
+menuContact.addEventListener("click", openContactModal);
+
+function openContactModal(){
+    document.getElementById("contactModal").style.display = "block";
+};
+
+
+closeModalbutton.addEventListener("click", closeContactModal);
+
+function closeContactModal(){
+    document.getElementById("contactModal").style.display = "none";
+};
+
+
+
+
+
